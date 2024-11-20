c50542654d027a7b31812a8e53a02135457f6c1f
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-20 16:14:58 +0800

    Header and Main setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+function Header(){
+    return(
+        <header className="z-20 sticky top-0 w-full bg-amber-50 flex flex-row border-b-2 border-b-black">
+            <div className="w-full p-4 flex flex-row justify-between">
+                <div>
+                    <p>Vaughn</p>
+                </div>
+
+                <div>
+                    <ul className="flex gap-8">
+                        <li>About Me</li>
+                        <li>What I do</li>
+                        <li>Experiences</li>
+                    </ul>
+                </div>
+            </div>
+
+        </header>
+    );
+}
+
+export default Header;
