0ba9217c12fae8782a7b144bc145b36b50ad20e2
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-20 16:13:56 +0800

    Header and Main setup

---
+++ src/components/Main/About.jsx
@@ -0,0 +1,16 @@
+
+function About(){
+    return(
+        <div className="h-screen grid grid-cols-2 grid-rows-1">
+            <div className="bg-dotted-pattern">
+                
+            </div>
+
+            <div className="flex flex-col justify-center items-center text-justify p-8">
+                Intro
+            </div>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import About from './About.jsx'
+
+function Main(){
+    return(
+        <div className='z-10 w-full min-h-screen'>
+            <About/>
+        </div>
+    );
+}
+
+export default Main;
