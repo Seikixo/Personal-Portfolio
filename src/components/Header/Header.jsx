c57a5d83137831fd9f326121c66f2d13f23e85fe
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-12-13 09:29:01 +0800

    Minor Fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,12 +7,10 @@ function Header() {
     return (
         <header className="flex flex-row z-40 top-0 w-full bg-custom-beige border-b-4 border-b-black md:sticky">
             <div className="w-full p-6 flex flex-row justify-between">
-                {/* Logo */}
-                <div className="font-heading text-2xl font-bold tracking-wider">
-                    <p>It Fitz</p>
+                <div className="font-heading text-2xl font-extrabold tracking-wider">
+                    <p>It fitz!</p>
                 </div>
 
-                {/* Navigation Links for larger screens */}
                 <div className="flex md:hidden">
                     <ul className="flex flex-wrap gap-8 font-body tracking-widest">
                         <li><a href="#about-me" className="text-custom-orange hover:text-black">About Me</a></li>
